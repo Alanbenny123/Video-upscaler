31884456ee3cd953b17b490367816e054e29023f
Alanbenny123 <alanbenny123@gmail.com>
2025-05-24 20:21:56 +0530

    c

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeWrapper from "./components/ThemeWrapper";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,7 +34,6 @@ export default function RootLayout({
           <main className="w-full">{children}</main>
         </ThemeWrapper>
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
